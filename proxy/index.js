38f9f7f5812c480c5c723c096adac892f93baaf6
Muhammad Mosaad <mm458@njit.edu>
2019-01-04 11:31:57 -0500

    freshened proxy files

--- proxy/index.js
+++ proxy/index.js
@@ -1,19 +1,18 @@
 const express = require("express");
-const router = require("./routes.js");
 const proxy = require("http-proxy-middleware");
-const morgan = require("morgan");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(morgan("combined"));
-
-app.set("view engine", "ejs");
 
 app.use("/:id", express.static(path.join(__dirname, "../views")));
-app.use("/api", proxy({ target: "http://ec2-54-174-166-132.compute-1.amazonaws.com/" }));
-// app.use("/", router);
+
+app.use(
+  "/api/carousel",
+  proxy({ target: "http://ec2-18-223-116-251.us-east-2.compute.amazonaws.com/" })
+);
+app.use("/api/similarlistings", proxy({ target: "http://localhost:3011/" }));
 
 app.listen(3000, () => console.log("Proxy Server Working!!!"));
